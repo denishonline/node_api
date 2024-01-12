6938fdee2203b185cb82178f110b1da7adf51055
Denish Patel <denish.patel@cygnet.com>
2024-01-12 12:41:51 +0530

    PMS: elasticsearch logging

--- src/controllers/switchController.js
+++ src/controllers/switchController.js
@@ -1,24 +1,24 @@
 const switchService = require("../services/switchService")
 
-const createSwitch = async (req, res) => {
+const createSwitch = async (req, res, next) => {
   try {
     const switchRow = await switchService.createSwitch(req.body)
     return res.status(201).json(switchRow)
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    next(error)
   }
 }
 
-const getAllSwitches = async (req, res) => {
+const getAllSwitches = async (req, res, next) => {
   try {
     const switches = await switchService.getAllSwitches()
     return res.status(200).json(switches)
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    next(error)
   }
 }
 
-const getSwitchById = async (req, res) => {
+const getSwitchById = async (req, res, next) => {
   const id = req.params.id
   try {
     const switchRow = await switchService.getSwitchById(id)
@@ -27,11 +27,11 @@ const getSwitchById = async (req, res) => {
     }
     return res.status(200).json(switchRow)
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    next(error)
   }
 }
 
-const updateSwitch = async (req, res) => {
+const updateSwitch = async (req, res, next) => {
   const id = req.params.id
   const updatedData = req.body
   try {
@@ -41,11 +41,11 @@ const updateSwitch = async (req, res) => {
     }
     return res.status(200).json(updatedSwitch)
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    next(error)
   }
 }
 
-const deleteSwitch = async (req, res) => {
+const deleteSwitch = async (req, res, next) => {
   const id = req.params.id
   try {
     const deletedSwitch = await switchService.deleteSwitch(id)
@@ -54,7 +54,7 @@ const deleteSwitch = async (req, res) => {
     }
     return res.status(200).json({ message: "Switch deleted successfully" })
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    next(error)
   }
 }
 
